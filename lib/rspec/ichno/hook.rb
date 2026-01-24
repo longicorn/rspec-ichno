e701f9bb8d8ae24ef417fd7d92e03574259930d5
longicorn <longicorn.c@gmail.com>
2026-01-24 21:24:14 +0900

    fix: Minimum functional implementation
    - exe/ichnotool: ichnoの結果からキャッシュを作成する
      - キャッシュ名が固定なので注意
    - ichnoのキャッシュを利用してテストをスキップできること

---
+++ lib/rspec/ichno/hook.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+def RSpec.ichno
+  @ichno ||= RspecIchno.new
+end
+
+RSpec.configuration.before(:suite) do |config|
+  RSpec.ichno.disable = !(['1', 'true'].include?(ENV['ICHNO']))
+end
+
+RSpec.configuration.around(:each) do |example|
+  RSpec.ichno.spec(example) do |ichno_spec|
+    if ichno_spec.skip? && example.metadata[:ichno] != false
+      example.execution_result.pending_fixed = true
+      example.execution_result.pending_message = 'skipped by rspec-ichno'
+      example.skip
+      next
+    end
+
+    example.run
+  end
+end
